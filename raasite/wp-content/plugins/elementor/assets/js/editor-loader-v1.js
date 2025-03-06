c217cfdc77474e6bceaa88490bc98e89574e3de2
hamidnoshady <64142695+hamidnoshady@users.noreply.github.com>
2025-03-06 18:03:57 +0330

    Updated/Added simply-static-1-1741269574

--- raasite/wp-content/plugins/elementor/assets/js/editor-loader-v1.js
+++ raasite/wp-content/plugins/elementor/assets/js/editor-loader-v1.js
@@ -1,6 +1,12 @@
 /*! elementor - v3.27.0 - 18-02-2025 */
-(()=>{"use strict";
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
 /*!*******************************************************!*\
   !*** ../core/editor/loader/v1/js/editor-loader-v1.js ***!
   \*******************************************************/
-window.elementor.start();})();
+
+
+window.elementor.start();
+/******/ })()
+;
+//# sourceMappingURL=editor-loader-v1.js.map
